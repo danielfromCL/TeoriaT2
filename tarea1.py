128ab0da8d87ddc29f2f93089372f7180d039043
danielfromCL <danielngc1@gmail.com>
2021-10-13 17:46:06 -0300

    comienzo

--- tarea1.py
+++ tarea1.py
@@ -1,5 +1,8 @@
 ER = open("regegxp.src", "r")
 texto = open('text.src', "r")
 
-def 
-hao
+#recorremos hasta llegar a un ")" sin \
+#volvemos hasta que encontremos un "(", vemos si tiene un "\" antes
+#si tiene -> seguimos hasta encontra otro "("
+#no tiene -> sabemos que lo que esta entre ese ( y el ) es una expresion
+
